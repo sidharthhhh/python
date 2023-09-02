b591aa46fb1457e6d71730b8fe206bb3393d273c
Sidharth Shukla <swayam1044@gmail.com>
2023-09-03 02:08:22 +0530

    day1

---
+++ day1/newStart.py
@@ -0,0 +1,22 @@
+# typecasting:- conversion of one datatype to another
+# a = '2'
+# b= '3'
+# print(int(a)+int(b));
+
+
+
+
+# ------------------------------------------------------------
+# taking a input in python
+# a = input("enter your name: ")
+# print("my name is ", a)
+
+# x = input("enter the value of x: ")
+# y = input("enter the value of y: ")
+# print(int(x)+int(y))
+
+
+
+# --------------------------------------------------------------------
+
+
