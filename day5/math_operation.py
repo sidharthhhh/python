9cdaabe2978976c4f655a144db0078e406cbb416
Sidharth Shukla <swayam1044@gmail.com>
2023-10-18 18:24:55 +0530

    function and mudule

---
+++ day5/math_operation.py
@@ -0,0 +1,11 @@
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return(x*y)
+def div(x,y):
+    if y!=0:
+        return x/y
+    else:
+        return "error: division by zero"
